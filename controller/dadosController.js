8518e0e96b8068da47859eea07d998ebaa2d4e09
Wesley Paiva <wesley.snybool.dev@gmail.com>
2019-12-02 21:23:55 -0300

    Commit do Projeto Final

---
+++ controller/dadosController.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+
+const resultadoApi = "https://produtoapi.azurewebsites.net/api/"
+
+const reqGetprodutos = "produto/GetProdutos"
+
+
+
+fetch(resultadoApi+reqGetprodutos)
+.then(response => response.json())
+.then(result => {
+  console.log(result);
+
+  
+
+})
+.catch(err => { 
+  // trata se alguma das promises falhar
+  console.error('Falha ao recuperar os dados da API', err); 
+});
